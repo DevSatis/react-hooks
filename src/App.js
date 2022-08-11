63debb9a31a4f28bb1db1f7d68c7596ec6eee049
DevSatis <sksatishk9@gmail.com>
2022-08-11 21:54:36 +0530

    UseStateHooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import './App.css'
 import ClassComponent from './ClassComponent'
+import UseStateHook from './UseStateHook'
 
 const App = () => {
   return (
-    <div>
-      <ClassComponent/>
+    <div className='App'>
+      {/* <ClassComponent/> */}
+      <UseStateHook />
     </div>
   )
 }
