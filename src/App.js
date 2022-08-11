6ed6a762f7a2ef0e692407f902a6ee09d4800316
DevSatis <sksatishk9@gmail.com>
2022-08-11 21:41:49 +0530

    ClassComponent-for-state-Change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import ClassComponent from './ClassComponent'
 
 const App = () => {
   return (
     <div>
-      Hello Satish
+      <ClassComponent/>
     </div>
   )
 }
